557a5b39a1bfb2725e00c71209b563a68cb5b843
Danny Le Kim <dannylekim@gmail.com>
2019-01-12 17:01:25 -0500

    (feat) add words as a socket connection (?)

--- src/main/java/com/danken/application/controllers/WordBowlController.java
+++ src/main/java/com/danken/application/controllers/WordBowlController.java
@@ -3,49 +3,42 @@ package com.danken.application.controllers;
 import com.danken.business.WordBowlInputState;
 import com.danken.sessions.GameSession;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
 
 import java.util.List;
 
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
-@RestController
+@Controller
 @Slf4j
-@RequestMapping("/wordbowl")
 public class WordBowlController {
 
     private GameSession gameSession;
-    private SimpMessagingTemplate template;
-
 
     @Inject
-    public WordBowlController(SimpMessagingTemplate template, GameSession userGameSession) {
-        this.template = template;
+    public WordBowlController(GameSession userGameSession) {
         this.gameSession = userGameSession;
     }
 
-    @RequestMapping(method = POST)
-    public WordBowlInputState addWords(@RequestBody List<String> inputWords ) {
+    @MessageMapping("/room/{roomCode}/addWords")
+    @SendTo("/room/{roomCode/addWords")
+    public WordBowlInputState addWords(@RequestBody List<String> inputWords) {
         var currentRoom = gameSession.getCurrentRoom();
         var game = currentRoom.getGame();
 
-        if(game == null){
+        if (game == null) {
             throw new IllegalStateException("Game hasn't started yet");
         }
 
         game.addWordBowl(inputWords, gameSession.getPlayer());
-        template.convertAndSend("/room/" + currentRoom.getRoomCode() + "/game", game.getState());
+//        template.convertAndSend("/room/" + currentRoom.getRoomCode() + "/game", game.getState());
 
         return game.getState();
     }
-
-
-
 
 
 }
