a07520223fa8de7a4cad256bc40db5b02e42a9a1
Danny Le Kim <dannylekim@gmail.com>
2019-07-13 23:41:54 -0400

    fix: revert effort to send word to only user

--- src/main/java/com/danken/application/config/WebSocketConfig.java
+++ src/main/java/com/danken/application/config/WebSocketConfig.java
@@ -1,24 +1,16 @@
 package com.danken.application.config;
 
-import java.util.Map;
-
 import javax.inject.Inject;
-import javax.servlet.http.HttpSession;
 
 import com.danken.interceptors.WebSocketSessionInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.Session;
 import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableScheduling
@@ -40,18 +32,7 @@ public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfig
 
     @Override
     public void configureStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/socket").setAllowedOrigins("*").setHandshakeHandler(new DefaultHandshakeHandler() {
-
-            //Get sessionId from request and set it in Map attributes
-            public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
-                                           Map attributes) throws Exception {
-                if (request instanceof ServletServerHttpRequest) {
-                    ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
-                    HttpSession session = servletRequest.getServletRequest().getSession();
-                    attributes.put("sessionId", session.getId());
-                }
-                return true;
-            }}).withSockJS();
+        registry.addEndpoint("/socket").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
